30e2e16d48157cfc4bc7a6ef61421b2249a4797f
Franco Ferreyra <24francoferreyra@gmail.com>
2023-02-09 10:54:13 -0300

    home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Home.module.css'
+function Home() {
+  return (
+    <section className={styles.box}>
+      <h3>Hola!, mi nombre es</h3>
+      <h1>Franco Ferreyra</h1>
+      <h4>Full Stack Developer</h4>
+    </section>
+  )
+}
+
+export default Home
