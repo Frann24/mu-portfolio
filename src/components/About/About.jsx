ac1c1434043026e330556a7fb4015b670bc527d1
Franco Ferreyra <24francoferreyra@gmail.com>
2023-02-11 15:34:20 -0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,11 +7,9 @@ function About() {
       <h3>About me</h3>
       <div>
         <p>
-          Soy un desarrollador de software en ascenso con una pasión por crear soluciones digitales atractivas y eficientes.
+        Soy un desarrollador de software en ascenso con una pasión por crear soluciones digitales atractivas y eficientes. Con una formación sólida en programación y desarrollo de aplicaciones web, estoy ansioso por aplicar mis habilidades en proyectos emocionantes y desafiantes.
         </p> 
-        <p>Con una formación sólida en programación y desarrollo de aplicaciones web, estoy ansioso por aplicar mis habilidades en proyectos emocionantes y desafiantes.</p>
-        <p>Me esfuerzo por mantenerme al tanto de las últimas tendencias y tecnologías en el campo para ofrecer soluciones innovadoras y efectivas a mis clientes.</p>
-        <p>Fuera del trabajo, disfruto la musica, los deportes y pasar tiempo con mi familia y amigos.</p>
+        <p>Me esfuerzo por mantenerme al tanto de las últimas tendencias y tecnologías en el campo para ofrecer soluciones innovadoras y efectivas a mis clientes. Fuera del trabajo, disfruto la música, los deportes y pasar tiempo con mi familia y amigos.</p>
       </div>
     </section>
   )
