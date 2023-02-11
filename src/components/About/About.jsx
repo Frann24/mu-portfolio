ec95f60fb32d5a053379d8a7acaaa634482830eb
Franco Ferreyra <24francoferreyra@gmail.com>
2023-02-11 15:03:20 -0300

    about me

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './About.module.css'
+
+function About() {
+  return (
+    <section className={styles.about}>
+      <h3>About me</h3>
+      <div>
+        <p>
+          Soy un desarrollador de software en ascenso con una pasión por crear soluciones digitales atractivas y eficientes.
+        </p> 
+        <p>Con una formación sólida en programación y desarrollo de aplicaciones web, estoy ansioso por aplicar mis habilidades en proyectos emocionantes y desafiantes.</p>
+        <p>Me esfuerzo por mantenerme al tanto de las últimas tendencias y tecnologías en el campo para ofrecer soluciones innovadoras y efectivas a mis clientes.</p>
+        <p>Fuera del trabajo, disfruto la musica, los deportes y pasar tiempo con mi familia y amigos.</p>
+      </div>
+    </section>
+  )
+}
+
+export default About
